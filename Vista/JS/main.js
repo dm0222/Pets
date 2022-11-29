1476169d458554955f0297e5142288bb5a11abd3
David Miranda <dfmirandad@correo.udistrital.edu.co>
2022-11-28 21:56:48 -0500

    Correccion del main.js

--- Vista/JS/main.js
+++ Vista/JS/main.js
@@ -1,21 +1,20 @@
 let validarMenuD = 0
 
 $(".submit02").click(function () {
-    if (validarMenuD == 0) {
-        $(".contenedor-derecha-registro").css("display", "block"), function () {
+    switch (validarMenuD){
+        case 0:
+            $(".contenedor-derecha-registro").css("display", "block");
             $(".submit02").css("background", "#F0F0F0");
             $(".submit02").css("color", "#36A420");
-            validarMenuD = 1
-        };
-
-    };
-    if (validarMenuD == 1) {
-        $(".contenedor-derecha-registro").css("display", "none"), function () {
+            validarMenuD = 1;
+        break;
+        case 1:
+            $(".contenedor-derecha-registro").css("display", "none");
             $(".submit02").css("background", "#36A420");
             $(".submit02").css("color", "#FFF");
-            validarMenuD = 0
-        };
-    };
+            validarMenuD = 0;
+        break;
+    }
 });
 
 $(".cerrar").click(function () {
