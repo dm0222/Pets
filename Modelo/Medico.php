726fe9f93b17b9bb4c0c75a22393335f4ca3a57a
dm0222 <dfmirandad@correo.udistrital.edu.co>
2022-11-29 22:00:12 -0500

    Correcion de los constructores del medico y el administrador

--- Modelo/Medico.php
+++ Modelo/Medico.php
@@ -1,8 +1,8 @@
 <?php
     require_once 'Persona.php';
     class Medico extends Persona {
-        public  function __construct($Documento, $Primer_Nombre, $Segundo_Nombre, $Primer_Apellido, $Segundo_Apellido, $Direccion, $Telefono, $Celular, $Email){
-            parent::__construct($Documento, $Primer_Nombre, $Segundo_Nombre, $Primer_Apellido, $Segundo_Apellido, $Direccion, $Telefono, $Celular, $Email);
+        public  function __construct($Documento){
+            parent::__construct($Documento);
         }
     }
 ?>
