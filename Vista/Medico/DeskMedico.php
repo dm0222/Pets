0f22e310bd7a1c490c95e9f36e275539bfce8b93
David Miranda <dfmirandad@correo.udistrital.edu.co>
2022-12-01 22:12:15 -0500

    adicion sweetalert

---
+++ Vista/Medico/DeskMedico.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glider-js@1.7.7/glider.min.css">
+    <link rel="stylesheet" href="../CSS/DeskStyle.css">
+    <link rel="icon" href="../Img/escritorio.png"/>
+    <title>Desk</title>
+</head>
+
+<body>
+
+    <!-- Video - background - inicio -->
+
+    <div class="video-background">
+        <video class="video-back" id="video" src="../Videos/Verde.mp4" muted autoplay loop playsinline ></video>
+    </div>
+
+    
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.8.0/dist/chart.min.js"></script>
+    <script
+        src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0/dist/chartjs-plugin-datalabels.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.debug.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/glider-js@1.7.7/glider.min.js"></script>
+    <script src="../Vista/JS/jquery-3.6.1.min.js"></script>
+    <script src="../Vista/JS/main.js"></script>
+</body>
+
+</html>
